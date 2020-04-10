25933114a3099233b13bae1ad59be1f30a46730a
Владислав <163vsk@gmail.com>
2020-04-10 19:47:37 +0400

    Получение данных о предмете с сервера

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.get('/', (req, res)=>{
 //Чисто для примера
 app.get('/instr', (req, res) => {
     res.json([
-        {id:1, species: 'Гитара', name: 'Fender Stratocaster HSS', price: 150000},
+        {id:1, species: 'Гитара', name: 'Kramer JK-1000', price: 60000},
         {id:2, species: 'Бас-гитара', name: 'Fender Precision Bass', price: 120000},
         {id:3, species: 'Укулеле', name: 'Flight', price: 3000},
         {id:4, species: 'Синтезатор', name: 'Roland SY-300', price: 70000}
@@ -36,9 +36,28 @@ app.get('/instr', (req, res) => {
 });
 
 app.get('/instrument/:id', (req, res) => {
-
+    console.log(`Instrument id:${req.params.id}`)
+    res.json(getInstrument(req.params.id))
 })
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
+
+function getInstrument(id) {
+    const instrument = {id:-1, species: 'Ошибка', name: 'Ошибка', description: 'Ошибка', price: 0};
+    console.log(id)
+    switch(Number(id)){
+        case 1: {
+            instrument.id = 1;
+            instrument.species = 'Гитара';
+            instrument.name = 'Kramer JK-1000';
+            instrument.description = 'Шред-машина';
+            instrument.price = 60000;
+            break;
+        }
+        default:
+            break;
+    }
+    return instrument;
+}
--- src/InstrumentPage/InstrumentPage.js
+++ src/InstrumentPage/InstrumentPage.js
@@ -1,10 +1,10 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Link } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
+import {Link} from 'react-router-dom';
+import {makeStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import { Card, Button } from '@material-ui/core';
+import {Card, Button} from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import {Routes} from '../routes';
 
@@ -17,7 +17,7 @@ const useStyles = makeStyles(theme => ({
         textAlign: "center",
         color: theme.palette.text.secondary
     },
-    whiteContainer:{
+    whiteContainer: {
         backgroundColor: theme.palette.text.secondary
     },
     image: {
@@ -37,7 +37,7 @@ function InstrumentPage(props) {
     const [loading, setLoading] = useState(true);
 
     /*useEffect(() => {
-        fetch(Routes.Instrument + props.match.id)
+        fetch(Routes.Instrument + props.match.params.id)
             .then(response => response.json())
             .then(instrument => {
                 setInstrument(instrument);
@@ -49,17 +49,18 @@ function InstrumentPage(props) {
     return (
         <div className="container">
             <Grid className={'bg-white border border-primary'} container spacing={3}>
-                <Grid item xs={7}>
+                {loading ? null : <React.Fragment><Grid item xs={7}>
                     <img className={classes.image} src={require('../placeholder.png')}/>
                 </Grid>
-                <Grid item xs={5}>
-                    <h1>Инструмент {props.match.params.id}</h1>
-                    <h2>100500</h2>
-                    <p>Lorem ipsum dolor sit amet</p>
-                    <Button variant="contained" color="primary">
-                        В КОРЗИНУ
-                    </Button>
-                </Grid>
+                    <Grid item xs={5}>
+                        <h6>Муз</h6>
+                        <h1>Инструмент {props.match.params.id}</h1>
+                        <h2>100500</h2>
+                        <p>Lorem ipsum dolor sit amet</p>
+                        <Button variant="contained" color="primary">
+                            В КОРЗИНУ
+                        </Button>
+                    </Grid></React.Fragment>}
             </Grid>
         </div>
     );
