f562a8b2c5c44aa0dd371f345dbc02d60562e529
charlez136 <61160821+charlez136@users.noreply.github.com>
2020-04-18 17:16:23 +0300

    fix fontawesome iss size

--- src/contactPage/contactPage.js
+++ src/contactPage/contactPage.js
@@ -13,28 +13,28 @@ function ContactPage(props) {
         <h1 className="text-center text-uppercase">контакты</h1>
         <p className="text-center w-75 m-auto">Здравствуйте, здесь вы найдете информацию, благодаря которой вы можете связаться с нами!</p>
         <div className="row">
-          <div className="col-sm-12 col-md-6 col-lg-3 my-5">
+          <div className="col-sm-12 col-md-4 col-lg-4 my-5">
             <div className="card border-0">
                <div className="card-body text-center">
-                 < FontAwesomeIcon icon={faPhone} aria-hidden="true"/>
+                 < FontAwesomeIcon icon={faPhone} aria-hidden="true" size="3x"/>
                  <h2 className="text-uppercase mb-5">наши телефоны</h2>
                  <p>+79172337480</p>
                </div>
              </div>
           </div>
-          <div className="col-sm-12 col-md-6 col-lg-3 my-5">
+          <div className="col-sm-12 col-md-4 col-lg-4 my-5">
             <div className="card border-0">
                <div className="card-body text-center">
-                 < FontAwesomeIcon icon={faMapMarkerAlt} aria-hidden="true"/>
-                 <h2 className="text-uppercase mb-5">местоположение</h2>
+                 < FontAwesomeIcon icon={faMapMarkerAlt} aria-hidden="true" size="3x"/>
+                 <h2 className="text-uppercase mb-5">мы здесь</h2>
                 <address>Самарский университет</address>
                </div>
              </div>
           </div>
-          <div className="col-sm-12 col-md-6 col-lg-3 my-5">
+          <div className="col-sm-12 col-md-4 col-lg-4 my-5">
             <div className="card border-0">
                <div className="card-body text-center">
-                  < FontAwesomeIcon icon={faGlobe} aria-hidden="true"/>
+                  < FontAwesomeIcon icon={faGlobe} aria-hidden="true" size="3x"/>
                  <h2 className="text-uppercase mb-5">email</h2>
                  <p>nashmail@mail.ru</p>
                </div>
