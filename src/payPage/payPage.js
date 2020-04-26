507beb0059e2f603a7a31cef3325b0284d42828a
ArhireyskiyOleg <61161066+ArhireyskiyOleg@users.noreply.github.com>
2020-04-26 13:09:46 +0400

    Update payPage.js

--- src/payPage/payPage.js
+++ src/payPage/payPage.js
@@ -4,29 +4,103 @@ import { faCcPaypal } from "@fortawesome/free-brands-svg-icons";
 import { faCcVisa } from "@fortawesome/free-brands-svg-icons";
 import { faCcMastercard } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-function PayPage(props) {
-    return (
-        <div className="container">
-        <h1 className="text-center text-uppercase">Варианты оплаты в интернет-магазине музыкальных инструментов</h1>
-        <p className="text-center w-75 m-auto">В нашем интернет-магазине вам доступно несколько удобных способов оплаты:</p>
-        <ul>
-            <li>Наличными в кассе магазина при самовывозе</li>
-            <li>Безналичным способом по выставленному нами счету</li>
-            <li>Пластиковой картой в кассе магазина при самовывозе</li>
-            <li>Пластиковыми картами в режиме онлайн:</li>
-        </ul>
-        <ul>
-
-        </ul>
-        <p className="text-center w-75 m-auto"> Для выбора оплаты товара с помощью банковской карты на соответствующей странице необходимо нажать кнопку «Оплата заказа
-           банковской картой". Оплата происходит через ПАО СБЕРБАНК с использованием Банковских карт следующих платежных систем: </p> 
-        <p>PayPal <FontAwesomeIcon icon={faCcPaypal} aria-hidden="true" size="3x"/> </p>
-        <p>Visa International <FontAwesomeIcon icon={faCcVisa} aria-hidden="true" size="3x"/></p>
-        <p>Mastercard Worldwide <FontAwesomeIcon icon={faCcMastercard} aria-hidden="true" size="3x"/></p>
+import { faCcAmazonPay } from "@fortawesome/free-brands-svg-icons";
+import { faCcApplePay } from "@fortawesome/free-brands-svg-icons";
+import { makeStyles } from '@material-ui/core/styles';
+import { createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    paper: {
+        height: 140,
+        width: 200,
+        textAlign: 'center',
+        padding: theme.spacing(4),
+    },
+  }));
+let theme = createMuiTheme({
+    typography: {
+        useNextVariants: true,
+        fontWeightMedium: 500,
+        h1:{
+            fontWeight: '500',
+            fontSize: '35px',
+            lineHeight: '2'
+        },
+        body1:{
+            fontSize: '18px',
+        },
+        overline:{
+            fontStyle: 'italic'
+        },
+        h3:{
+            fontSize: '25px',
+            fontWeight: '500',
+        }
         
-
-</div>
+    },
+});
+function PayPage(props) {
+    const classes = useStyles();
+    const [spacing, setSpacing] = React.useState(5);
+    
+    return (
+        <ThemeProvider theme={theme}>
+        <div className={classes.root}>
+        <Typography variant="h1" paragraph>Варианты оплаты в интернет-магазине "Музыкальный Маг"</Typography>
+        <Typography variant="h3" paragraph>В нашем интернет-магазине вам доступно несколько удобных способов оплаты:</Typography>
+        <div className="w-100"></div>
+        <ul>
+            <li><Typography variant="body1">Наличными в кассе магазина при самовывозе</Typography></li>
+            <li><Typography variant="body1">Безналичным способом по выставленному нами счету</Typography></li>
+            <li><Typography variant="body1">Пластиковой картой в кассе магазина при самовывозе</Typography></li>
+            <li><Typography variant="body1" paragraph>Пластиковыми картами в режиме онлайн:</Typography></li>
+            <Typography variant="body1"> Для выбора оплаты товара с помощью банковской карты на соответствующей странице необходимо нажать кнопку «Оплата заказа
+           банковской картой". Оплата происходит через ПАО СБЕРБАНК с использованием Банковских карт следующих платежных систем:</Typography>
+        </ul>
+        <Grid container className={classes.root} >
+        <Grid item xs={12}>
+          <Grid container justify="center" spacing={spacing}>
+              <Grid item>
+                <Paper className={classes.paper}>
+                <FontAwesomeIcon icon={faCcPaypal} aria-hidden="true" size="3x" />
+                <Typography variant="h3">PayPal</Typography>
+                </Paper>
+              </Grid>
+              <Grid item>
+                <Paper className={classes.paper}>
+                <FontAwesomeIcon icon={faCcVisa} aria-hidden="true" size="3x"/>
+                <Typography variant="body1">Visa International</Typography>
+                </Paper>
+              </Grid>
+              <Grid item>
+                <Paper className={classes.paper}>
+                <FontAwesomeIcon icon={faCcMastercard} aria-hidden="true" size="3x"/>
+                <Typography variant="body1">Mastercard Worldwide</Typography>
+                </Paper>
+              </Grid>
+              <Grid item>
+                <Paper className={classes.paper}>
+                <FontAwesomeIcon icon={faCcAmazonPay} aria-hidden="true" size="3x"/>
+                <Typography variant="body1">Amazon Pay</Typography>
+                </Paper>
+              </Grid>
+              <Grid item>
+                <Paper className={classes.paper}>
+                <FontAwesomeIcon icon={faCcApplePay} aria-hidden="true" size="3x"/>
+                <Typography variant="body1">Apple Pay</Typography>
+                </Paper>
+              </Grid>
+          </Grid>
+        </Grid>
+        </Grid>
+        </div>
+        </ThemeProvider>
     );
 }
 
